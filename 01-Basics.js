63213427df810678f3faa52242fe3987f82e841a
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2024-07-11 19:02:47 +0500

    some code is removed

--- 01-Basics.js
+++ 01-Basics.js
@@ -1,7 +1,5 @@
 console.log("Hello World in java script");
-let firs_number = 10;
-let second_number = 20;
-console.log(firs_number + second_number);
+
 
 function sum (a,b) {
     console.log(a + b);
